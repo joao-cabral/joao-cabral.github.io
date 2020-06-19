39f261b10dc5c47493ae8ac7694e257924b960a2
João Cabral <38953911+joao-cabral@users.noreply.github.com>
2020-06-19 10:36:55 -0400

    First Version

---
+++ precache-manifest.2d1abae8853fd0b5e638639fcc990f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2153183dbf31e00a3f5a86f262ff154e",
+    "url": "/clone-eldorado/index.html"
+  },
+  {
+    "revision": "dd146d8d42ac665be80a",
+    "url": "/clone-eldorado/static/css/main.76a6d3db.chunk.css"
+  },
+  {
+    "revision": "53f62cfab9614cac1c76",
+    "url": "/clone-eldorado/static/js/2.82e91a2a.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/clone-eldorado/static/js/2.82e91a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd146d8d42ac665be80a",
+    "url": "/clone-eldorado/static/js/main.2e8dd45c.chunk.js"
+  },
+  {
+    "revision": "fc2cd2ea94bc4e7c161c",
+    "url": "/clone-eldorado/static/js/runtime-main.27bb0b31.js"
+  }
+]);
